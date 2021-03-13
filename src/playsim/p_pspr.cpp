34597d19f3dcbbb6431c7567425989469f2e7777
RaveYard <MrRaveYard@gmail.com>
2021-03-13 13:53:47 +0100

    Fix new overlay manipulation which are not called from psprites

--- src/playsim/p_pspr.cpp
+++ src/playsim/p_pspr.cpp
@@ -751,7 +751,7 @@ DEFINE_ACTION_FUNCTION(AActor, A_OverlayScale)
 	PARAM_FLOAT(wy)
 	PARAM_INT(flags)
 	
-	if (!ACTION_CALL_FROM_PSPRITE() || ((flags & WOF_KEEPX) && (flags & WOF_KEEPY)))
+	if ((layer == 0 && !ACTION_CALL_FROM_PSPRITE()) || ((flags & WOF_KEEPX) && (flags & WOF_KEEPY)))
 		return 0;
 
 	DPSprite *pspr = self->player->FindPSprite(((layer != 0) ? layer : stateinfo->mPSPIndex));
@@ -783,7 +783,7 @@ DEFINE_ACTION_FUNCTION(AActor, A_OverlayRotate)
 	PARAM_ANGLE(degrees)
 	PARAM_INT(flags)
 
-	if (!ACTION_CALL_FROM_PSPRITE())
+	if (layer == 0 && !ACTION_CALL_FROM_PSPRITE())
 		return 0;
 
 	DPSprite *pspr = self->player->FindPSprite(((layer != 0) ? layer : stateinfo->mPSPIndex));
@@ -811,7 +811,7 @@ DEFINE_ACTION_FUNCTION(AActor, A_OverlayPivot)
 	PARAM_FLOAT(wy)
 	PARAM_INT(flags)
 
-	if (!ACTION_CALL_FROM_PSPRITE() || ((flags & WOF_KEEPX) && (flags & WOF_KEEPY)))
+	if ((layer == 0 && !ACTION_CALL_FROM_PSPRITE()) || ((flags & WOF_KEEPX) && (flags & WOF_KEEPY)))
 		return 0;
 
 	DPSprite *pspr = self->player->FindPSprite(((layer != 0) ? layer : stateinfo->mPSPIndex));
@@ -931,7 +931,7 @@ DEFINE_ACTION_FUNCTION(AActor, A_OverlayPivotAlign)
 	PARAM_INT(halign);
 	PARAM_INT(valign);
 
-	if (!ACTION_CALL_FROM_PSPRITE())
+	if (layer == 0 && !ACTION_CALL_FROM_PSPRITE())
 		return 0;
 
 	DPSprite *pspr = self->player->FindPSprite(((layer != 0) ? layer : stateinfo->mPSPIndex));
